14598ba4a8ea88cc763a91aea0751a9f40ac6279
Marina Kontalexi <kontalexim@gmail.com>
2023-03-23 21:46:27 +0200

    b

--- src/rest.py
+++ src/rest.py
@@ -58,7 +58,8 @@ def register():
     me.register_node_to_ring(pk, ip)
     if me.current_id_count == total - 1:
         for x in me.ring:
-            if x not in me.ring: print("wtf")
+            if x not in me.ring: print(type(x))
+            print(x)
             requests.post("http://" + me.ring[x][1] + '/newnode/', data = jsonpickle.encode(me.ring))
             # print("broadcast to: ", me.ring[x][0])      
     return "0"
