023b481c47f2c097fd579c48742c10553ad84f6d
Marina Kontalexi <kontalexim@gmail.com>
2023-03-24 22:21:40 +0200

    f

--- src/rest.py
+++ src/rest.py
@@ -38,15 +38,15 @@ def cli_function(name):
     if ip != master_node: requests.get("http://" + ip  + my_port + "/login/")
     time.sleep(20)
     f = open(project_path + "5nodes/transactions{}.txt".format(me.ring[me.wallet.address][0]), "r")
-    s = " "
+    s = s = f.readline()
     t = time.time()
     while s != "":
-        s = f.readline()
         [r, amount] = s.split()
         rcv = r[2:]
         if int(rcv) >= total: continue
         requests.get("http://" + ip  + my_port + "/t?to=" + rcv + '&amount=' + amount)
         # time.sleep(10)
+        s = f.readline()
     print("Time", time.time() - t)
 #.......................................................................................
 
