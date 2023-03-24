686a66c0c72a1a376448048290b0e1e3454158e1
Marina Kontalexi <kontalexim@gmail.com>
2023-03-24 17:55:22 +0200

    quickfix

--- src/rest.py
+++ src/rest.py
@@ -81,7 +81,7 @@ def register():
     ip = dict["ip"]
     if pk in me.ring:
         if me.ring[pk][1] == ip:
-            requests.post("http://" + ip + '/genesis/', data = jsonpickle.encode((chain, me.wallet.utxos)))
+            requests.post("http://" + ip + '/genesis/', data = jsonpickle.encode((me.ring, chain, me.wallet.utxos)))
         else:
             print("ERROR: Public key already registered")
             requests.post("http://" + ip + '/login/')
