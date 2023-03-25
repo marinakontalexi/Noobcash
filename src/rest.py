7f962d82b13b85dc0d1906df9d3518e77e23a2ce
Marina Kontalexi <kontalexim@gmail.com>
2023-03-25 22:09:49 +0200

    :

--- src/rest.py
+++ src/rest.py
@@ -56,7 +56,7 @@ def queue_function(qevent):
                     print(colored("t was received", color_buffer)) 
                     me.add_transaction_to_block(t)
                     print(colored("t was added to block. Block size is " + str(len(me.currentBlock.listOfTransactions)), color_buffer))
-                    if len(me.currentBlock.listOfTransactions) == block.capacity
+                    if len(me.currentBlock.listOfTransactions) == block.capacity:
                         p = threading.Thread(target = mine_function, args=(blc_rcv,), daemon=True)
                         p.start()
 
