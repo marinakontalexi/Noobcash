271448eef4a82a2324524f42921e0eef51670573
Marina Kontalexi <kontalexim@gmail.com>
2023-03-26 17:43:15 +0300

    5 to 10

--- src/rest2.py
+++ src/rest2.py
@@ -82,7 +82,7 @@ def cli_function():
         print(colored("Posting transaction: " + s, color_cli))
         requests.get("http://" + ip  + my_port + "/t?to=" + rcv + '&amount=' + amount)
         log += 1
-        sleep = randint(2, 5)
+        sleep = randint(5, 10)
         time.sleep(sleep)
         s = f.readline()
     print(colored("I posted " + str(log) + " transactions", color_cli))
