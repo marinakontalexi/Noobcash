f66bf5e2d9c970e26d685554f8fcb1c1f0828b67
Marina Kontalexi <kontalexim@gmail.com>
2023-03-26 08:43:22 +0300

    s

--- src/rest2.py
+++ src/rest2.py
@@ -75,11 +75,11 @@ def mine_function(event):
         requests.post("http://" + me.ring[x][1] + '/newblock/', data = jsonpickle.encode(newblock))
 
 def cli_function():
-    time.sleep(10)
+    time.sleep(15)
     if ip != master_node: requests.get("http://" + ip  + my_port + "/login/")   
     queue = threading.Thread(target = queue_function, args=(qevent,), daemon=True)
     queue.start()
-    time.sleep(10)
+    time.sleep(5)
     f = open(project_path + "5nodes/transactions{}.txt".format(me.ring[me.wallet.address][0]), "r")
     s = f.readline()
     t = time.time()
